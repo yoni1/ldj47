417f2eb50173505372b58fb9c66e22fd669e2203
Eran Galili <geran@google.com>
2020-10-03 19:20:31 +0300

    The henchman can now rotate with the mouse (in an ugly way) and shoot fish

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rigidBody;
+    public Camera camera;
+
+    Vector2 movement;
+    Vector2 mousePosition;
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        mousePosition = camera.ScreenToWorldPoint(Input.mousePosition);
+        
+    }
+
+    private void FixedUpdate()
+    {
+        rigidBody.MovePosition(rigidBody.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        Vector2 lookDirection = mousePosition - rigidBody.position;
+        float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg - 180f;
+
+        rigidBody.rotation = angle; 
+    }
+}
+ 
---
+++ Assets/Shooting.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+
+    public Transform firePoint;
+    public GameObject fishPrefab;
+
+    public float fishForce = 5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject fish = Instantiate(fishPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody2D rigidBody = fish.GetComponent<Rigidbody2D>();
+        rigidBody.AddForce(firePoint.right * fishForce, ForceMode2D.Impulse);
+    }
+}
