1d2e30acdabd80da82dd0f428661a48397331d28
Alon Reznik <alonreznik@gmail.com>
2020-10-05 02:49:33 +0300

    add bullets to basic enemy controller

---
+++ Assets/Scripts/Enemies/Bullets/BulletDirectDown.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletDirectDown : EnemyBulletController
+{
+    private Vector3 worldVector = new Vector3();
+
+    float bulletForce = 12.4f;
+   
+
+    public override void ShootBullet()
+    {
+        Vector3 currentFirePoint = new Vector3(playerRb.transform.position.y, -1, 0);
+        bulletRb.AddForce(currentFirePoint*bulletForce, ForceMode2D.Impulse);
+    }
+}
