a436105343a1bcf8cac54cb5274caaf453a17872
Yoni <yoni@guardicore.com>
2020-10-04 15:11:38 +0300

    Scenes and sounds

---
+++ Assets/Scripts/OpeningCutscene.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class OpeningCutscene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        VideoPlayer player = GetComponent<VideoPlayer>();
+        player.url = System.IO.Path.Combine(Application.streamingAssetsPath, "OpeningCutscene.mp4");
+        player.loopPointReached += EndReached;
+        player.Play();
+    }
+
+    void NextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    void EndReached(VideoPlayer vp)
+    {
+        NextScene();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            NextScene();
+        }
+    }
+}
