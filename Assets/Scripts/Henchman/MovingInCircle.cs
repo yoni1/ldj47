949a69576278d28dce9c037b89805e179793aa1f
garlicalien@gmail.com <geran@google.com>
2020-10-04 16:54:52 +0300

    Not finished yet - but converting shoots to not use the mouse.

--- Assets/Scripts/Henchman/MovingInCircle.cs
+++ Assets/Scripts/Henchman/MovingInCircle.cs
@@ -40,6 +40,12 @@ public class MovingInCircle : MonoBehaviour
     [SerializeField]
     private Vector2 vector2_debug3;
 
+    public GameObject currentFirePoint;
+
+    private Dictionary<SpriteDirectionResolver.Direction, GameObject> dirToFirePoint;
+
+    public Sprite S;
+    public Sprite N;
 
     // Start is called before the first frame update
     void Start()
@@ -50,6 +56,15 @@ public class MovingInCircle : MonoBehaviour
         Vector3 radiusVector = (fishtank.transform.position - henchman.transform.position);
         radius = radiusVector.magnitude;
         angle = Mathf.Atan(radiusVector.y / radiusVector.x);
+
+        dirToFirePoint = new Dictionary<SpriteDirectionResolver.Direction, GameObject> {
+                {SpriteDirectionResolver.Direction.SW, this.gameObject.transform.GetChild(0).gameObject},
+                {SpriteDirectionResolver.Direction.NE, this.gameObject.transform.GetChild(1).gameObject},
+                {SpriteDirectionResolver.Direction.NW, this.gameObject.transform.GetChild(2).gameObject},
+                {SpriteDirectionResolver.Direction.SE, this.gameObject.transform.GetChild(3).gameObject}
+            };
+
+        currentFirePoint = dirToFirePoint[SpriteDirectionResolver.Direction.SW];
     }
 
     private Vector2 get_to_position()
@@ -106,6 +121,59 @@ public class MovingInCircle : MonoBehaviour
 
         if (diff < 0) { diff += 2 * Mathf.PI; }
         return diff < (Mathf.PI);
+    }
+
+    private void RotateSprite(Vector2 newPosition)
+    {
+        Vector2 fishTankPos = (Vector2)fishtank.transform.position;
+        Vector2 lookDirection = newPosition - fishTankPos;
+
+        float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg - 180f;
+
+        SpriteDirectionResolver.Direction direction = SpriteDirectionResolver.ResolveDirection(-angle);
+        SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+
+        switch (direction)
+        {
+            case SpriteDirectionResolver.Direction.W:
+                break;
+            case SpriteDirectionResolver.Direction.NW:
+                renderer.sprite = N;
+                currentFirePoint = dirToFirePoint[SpriteDirectionResolver.Direction.NW];
+                Debug.Log($"Sprite is now N, angle is {angle}");
+                renderer.flipX = false;
+                break;
+            case SpriteDirectionResolver.Direction.N:
+                break;
+            case SpriteDirectionResolver.Direction.NE:
+                renderer.sprite = N;
+                currentFirePoint = dirToFirePoint[SpriteDirectionResolver.Direction.NE];
+                Debug.Log($"Sprite is now N flip, angle is {angle}");
+                renderer.flipX = true;
+                break;
+            case SpriteDirectionResolver.Direction.E:
+                break;
+            case SpriteDirectionResolver.Direction.SE:
+                renderer.sprite = S;
+                currentFirePoint = dirToFirePoint[SpriteDirectionResolver.Direction.SE];
+                Debug.Log($"Sprite is now S flip, angle is {angle}");
+                renderer.flipX = true;
+                break;
+            case SpriteDirectionResolver.Direction.S:
+                break;
+            case SpriteDirectionResolver.Direction.SW:
+                renderer.sprite = S;
+                currentFirePoint = dirToFirePoint[SpriteDirectionResolver.Direction.SW];
+                Debug.Log($"Sprite is now S, angle is {angle}");
+                renderer.flipX = false;
+                break;
+        }
+
+        Vector2 shootDirection = newPosition - (Vector2)currentFirePoint.transform.position;
+        float shootAngle = Mathf.Atan2(shootDirection.y, shootDirection.x) * Mathf.Rad2Deg;
+
+        currentFirePoint.transform.rotation = Quaternion.Euler(0, 0, shootAngle);
+
     }
 
     private bool MovePlayer(Vector2 endPos)
