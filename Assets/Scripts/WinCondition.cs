4e3e0e2990141276ec607024e3103e6abab1dd77
Uri Shomroni <ushomroni@nvidia.com>
2020-10-06 00:52:26 +0300

    Adding win condition (timer + skip keys)

--- Assets/Scripts/WinCondition.cs
+++ Assets/Scripts/WinCondition.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class WinCondition : MonoBehaviour
 {
     public float stageLength;
+    public AudioClip winSound;
+    public float winWait;
 
     private float elapsedTime = 0.0f;
     // Start is called before the first frame update
@@ -13,13 +16,26 @@ public class WinCondition : MonoBehaviour
         
     }
 
+    public IEnumerator PlaySoundAndGoToNext()
+    {
+        AudioSource audio = GetComponent<AudioSource>();
+        audio.Stop();
+        audio.PlayOneShot(winSound);
+        yield return new WaitForSeconds(winWait);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
     // Update is called once per frame
     void Update()
     {
         elapsedTime += Time.deltaTime;
-        if (stageLength <= elapsedTime)
+        if ((stageLength <= elapsedTime) || (Input.GetKeyDown(KeyCode.RightBracket)))
         {
-            
+            StartCoroutine(PlaySoundAndGoToNext());
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftBracket))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
         }
     }
 }
