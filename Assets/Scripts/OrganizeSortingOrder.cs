3ba86481811d2c4f1a31ad52dba72f78ba6d965e
Yoni <yoni@guardicore.com>
2020-10-05 17:03:50 +0300

    Sorting order

---
+++ Assets/Scripts/OrganizeSortingOrder.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Assets.Scripts
+{
+    public class OrganizeSortingOrder : MonoBehaviour
+    {
+        private SpriteRenderer sp;
+
+        // Use this for initialization
+        void Start()
+        {
+            sp = GetComponent<SpriteRenderer>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            float bottom = sp.bounds.center.y + sp.bounds.extents.y;
+            sp.sortingOrder = -(int)(bottom * 100.0f);
+        }
+    }
+}
