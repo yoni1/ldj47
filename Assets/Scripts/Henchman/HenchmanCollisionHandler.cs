c37112daac9eb27f5b2b613be8c24b2f7d28aed4
garlicalien@gmail.com <geran@google.com>
2020-10-05 18:20:48 +0300

    Shots will now kill the henchman and cause game over.

--- Assets/Scripts/Henchman/HenchmanCollisionHandler.cs
+++ Assets/Scripts/Henchman/HenchmanCollisionHandler.cs
@@ -1,18 +1,57 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class HenchmanCollisionHandler : MonoBehaviour
 {
     public Sprite deadSprite;
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    public float deadBeforeGameOverSec = 1.5f;
+    public float gameOverBeforeRestartSec = 3.0f;
+
+    private bool isDead = false;
+    private float elapsedTime = -1f;
+
+    private Text gameOverText;
+
+    private void Start()
+    {
+        gameOverText = GameObject.Find("GameOverText").GetComponent<Text>();
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
     {
         switch (LayerMask.LayerToName(collision.gameObject.layer)) {
             case ("EnemyBullet"):
                 gameObject.GetComponent<MovingInCircle>().canMove = false;
                 gameObject.GetComponent<Shooting>().canShoot = false;
                 gameObject.GetComponent<SpriteRenderer>().sprite = deadSprite;
+                isDead = true;
                 break;
          }
+    }
+
+    void Update()
+    {
+        if (!isDead)
+            return;
+
+        if (elapsedTime < 0f)
+        {
+            elapsedTime = 0f;
+            return;
+        }
+
+        elapsedTime += Time.deltaTime;
+
+        if (elapsedTime >= deadBeforeGameOverSec && !gameOverText.enabled)
+        {
+            gameOverText.enabled = true;
+            return;
+        }
+
+        if (elapsedTime >= deadBeforeGameOverSec + gameOverBeforeRestartSec)
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
