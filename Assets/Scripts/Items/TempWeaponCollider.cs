583c75e351011512036d6dd3987145e10b5f745d
Uri Shomroni <ushomroni@nvidia.com>
2020-10-05 04:28:51 +0300

    Adding Eel powerup * Script for behavior (parameterized on weapon type and timeout) * Sprite * Prefab using the sprite and script + a collider and parameter

---
+++ Assets/Scripts/Items/TempWeaponCollider.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempWeaponCollider : MonoBehaviour
+{
+    public GameObject weaponType;
+    public float weaponDuration;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Shooting otherShooting = other.gameObject.GetComponent<Shooting>();
+        otherShooting.SetTemporaryWeapon(weaponType, weaponDuration);
+        Destroy(gameObject);
+    }
+}
